1ee6d3ad13d5998c19d10c8ac5fd74e8cd74afb6
wangji1 <wangji1@bngrp.com>
2022-02-06 18:11:45 +0800

    init project

---
+++ app/src/main/java/com/jj/livedatacalladapterfactory/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jj.livedatacalladapterfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
